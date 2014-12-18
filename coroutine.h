dd636176d9593fc162261944f0a60a31881813e5
wyy <wuyanyi09@gmail.com>
2014-12-18 15:27:16 +0800

    读代码

--- coroutine.h
+++ coroutine.h
@@ -1,6 +1,11 @@
 #ifndef C_COROUTINE_H
 #define C_COROUTINE_H
 
+#include <ucontext.h>
+
+#define STACK_SIZE (1024*1024)
+#define DEFAULT_COROUTINE 16
+
 #define COROUTINE_DEAD 0
 #define COROUTINE_READY 1
 #define COROUTINE_RUNNING 2
@@ -8,15 +13,19 @@
 
 struct schedule;
 
-typedef void (*coroutine_func)(struct schedule *, void *ud);
+typedef void (*coroutine_func)(struct schedule* S, void* arg);
 
-struct schedule * coroutine_open(void);
-void coroutine_close(struct schedule *);
+struct schedule* coroutine_open(void);
 
-int coroutine_new(struct schedule *, coroutine_func, void *ud);
-void coroutine_resume(struct schedule *, int id);
-int coroutine_status(struct schedule *, int id);
-int coroutine_running(struct schedule *);
-void coroutine_yield(struct schedule *);
+void coroutine_close(struct schedule* S);
+
+int  coroutine_new(struct schedule* S,coroutine_func func,void *arg);
+
+void coroutine_yield(struct schedule* S);
+
+void coroutine_resume(struct schedule* S,int id);
+
+int  coroutine_running(struct schedule *S);
+int  schedule_status(struct schedule* S);
 
 #endif
--- main.c
+++ main.c
@@ -10,8 +10,19 @@ foo(struct schedule * S, void *ud) {
 	struct args * arg = ud;
 	int start = arg->n;
 	int i;
-	for (i=0;i<5;i++) {
-		printf("coroutine %d : %d\n",coroutine_running(S) , start + i);
+	for (i=0;i<3;i++) {
+		printf("coroutine %d : %d\n", coroutine_running(S), start + i);
+		coroutine_yield(S);
+	}
+}
+
+static void
+foo2(struct schedule * S, void *ud) {
+	struct args * arg = ud;
+	int start = arg->n;
+	int i;
+	for (i=0;i<2;i++) {
+		printf("coroutine %d : %d\n", coroutine_running(S), start + i);
 		coroutine_yield(S);
 	}
 }
@@ -21,10 +32,13 @@ test(struct schedule *S) {
 	struct args arg1 = { 0 };
 	struct args arg2 = { 100 };
 
+    // 协程co1 ，对应的协程函数是 foo
 	int co1 = coroutine_new(S, foo, &arg1);
-	int co2 = coroutine_new(S, foo, &arg2);
+    // 协程co2 ，对应协程函数是 foo2
+	int co2 = coroutine_new(S, foo2, &arg2);
+
 	printf("main start\n");
-	while (coroutine_status(S,co1) && coroutine_status(S,co2)) {
+	while (schedule_status(S)) {
 		coroutine_resume(S,co1);
 		coroutine_resume(S,co2);
 	} 
@@ -33,8 +47,10 @@ test(struct schedule *S) {
 
 int 
 main() {
+    // 初始化协程调度器
 	struct schedule * S = coroutine_open();
 	test(S);
+    // 关闭协程调度器
 	coroutine_close(S);
 	
 	return 0;
